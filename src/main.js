19cd3828041ae98500bc96874267cba450ab7a20
Maxim Shashurin <waiyyshik@gmail.com>
2022-04-27 23:20:38 +0500

    Добавит модуль главной навигации

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import ProfileRatingElement from './view/profile-rating-view';
+import MainNavigationElement from './view/main-navigation-view';
 import { render } from './render';
 
 const header = document.querySelector('.header');
+const main = document.querySelector('.main');
 
 render(new ProfileRatingElement(),header);
+render(new MainNavigationElement(),main);
