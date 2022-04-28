6d7ba761c204d06ff8d800cbb082ab47c388c0f7
Maxim Shashurin <waiyyshik@gmail.com>
2022-04-28 21:07:07 +0500

    Добавляет модуль статистики в футер

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import ProfileRatingElement from './view/profile-rating-view';
 import MainNavigationElement from './view/main-navigation-view';
 import SortElement from './view/sort-view';
+import FooterStatisticsView from './view/footer-statistics-view';
 import { render } from './render';
 import FilmsPresenter from './presenter/films-presenter';
 
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
+const footer = document.querySelector('.footer');
 
 render(new ProfileRatingElement(),header);
 render(new MainNavigationElement(),main);
@@ -14,3 +16,5 @@ render(new SortElement(),main);
 const filmsPresenter = new FilmsPresenter();
 
 filmsPresenter.init(main);
+
+render(new FooterStatisticsView(),footer);
