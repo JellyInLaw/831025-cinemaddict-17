923bd012cebb12d95530ab5d7f40abacaa7b6254
Maxim Shashurin <waiyyshik@gmail.com>
2022-04-30 22:48:08 +0500

    Подключит модель

--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -5,13 +5,16 @@ import ShowMoreButtonView from '../view/show-more-button-view';
 
 export default class FilmsPresenter {
 
-  init = (filmsContainer) => {
+  init = (filmsContainer,cardModel) => {
+    this.cardModel = cardModel;
+    this.cards = [...this.cardModel.getCards()];
 
     render(new FilmsView(),filmsContainer);
     const filmsComponent = document.querySelector('.films-list__container');
 
-    for (let i = 0 ; i < 5 ; i++) {
-      render(new FilmCardView(),filmsComponent);
+    for (let i = 0 ; i < this.cards.length ; i++) {
+      render(new FilmCardView(this.cards[i]),filmsComponent);
+      console.log(this.cards[i]);
     }
     const placeForShowMoreButton = document.querySelector('.films-list');
     render(new ShowMoreButtonView(),placeForShowMoreButton);
